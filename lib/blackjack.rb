4e88613636a2e2c3e24b9c86d66d518bf37c7cc5
Aaron Klotnia <aklotnia@gmail.com>
2019-10-23 03:24:40 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -58,7 +58,7 @@ def runner
   hit?(card_total)
   display_card_total(card_total)
   until card_total > 21 do
-    hit?(card_total)
+    card_total = hit?(card_total)
     display_card_total(card_total)
   end
   end_game(card_total)
