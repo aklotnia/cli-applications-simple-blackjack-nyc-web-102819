b61d3697b858f197b304a6557b222352ad7f79e0
Aaron Klotnia <aklotnia@gmail.com>
2019-10-23 03:26:00 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -55,8 +55,6 @@ end
 def runner
   welcome
   card_total = initial_round
-  hit?(card_total)
-  display_card_total(card_total)
   until card_total > 21 do
     card_total = hit?(card_total)
     display_card_total(card_total)
